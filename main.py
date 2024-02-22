7980d4fa0d0eac1c6e0d6eb7ac8e544b363635bb
[Shivkumar Nagre] <[shivkumarnagre.sk@gmail.com]>
2024-02-22 22:01:58 +0530

    Update

--- main.py
+++ main.py
@@ -2,13 +2,39 @@ import pygsheets
 import pandas as pd
 from tkinter import *
 
-gc = pygsheets.authorize(service_file='Credential.json')
+def form():
+    root = Tk()
+    root.title("Details")
+    root.geometry('600x400')
+    root.resizable(0,0)  
+    Label(root,text = "Information", font='arial 20 bold').pack()
+    
+    fname_label = Label(root, text="Enter your first name:").pack()#.place(x= 32, y= 90)
+    fname_field = Entry(root, width= 30)#.place(x= 32, y= 90)
+    fname_field.pack()
 
-df = pd.DataFrame()
+    lname_label = Label(root, text="Enter your last name:").pack()
+    lname_field = Entry(root, width=30)
+    lname_field.pack()
 
-df['Name'] = ['John', 'Steve', 'Sarah', 'Mike']
-sh = gc.open('store_data')
+    email_label = Label(root, text="Enter your Email:").pack()
+    email_field = Entry(root, width=30)
+    email_field.pack()
 
-wks = sh[0]
+    btn = Button(root,text='Submit',  font='arial 15 bold', bg='pale violet red', command= lambda: submit(fname_field.get(), lname_field.get(), email_field.get()))
+    btn.pack()
+    root.mainloop()
 
-wks.set_dataframe(df,(1,1))
+def submit(fname, lname, email):
+    print(f"Submitted: {fname}, {lname}, {email}")
+    gc = pygsheets.authorize(service_file='Credential.json')
+    df = pd.DataFrame()
+    df['Firstame'] = [fname] 
+    df['Lastname'] = [lname]
+    df['Email'] = [email]
+    gsheet = gc.open('store_data')
+    wks = gsheet[0]
+   # wks.set_dataframe(df,(1,1))
+    wks.set_dataframe(df,(1,1))
+form()
+
